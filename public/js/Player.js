92ae080372c41ecc9d3449cd4b9f8bb1f73984df
Prasad Rathinasabapathi <rathina.prasad@gmail.com>
2012-12-01 14:59:46 -0700

    moved the files

---
+++ public/js/Player.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function Player() {
+  this.x = 100;
+  this.y = 100;
+}
+
+Player.prototype.draw = function(context) {
+   // alert("left------------");
+  ctx.fillStyle="#00FFFF";
+  context.fillRect(this.x, this.y, 32, 32);
+};
+
+Player.prototype.moveLeft = function() {
+ // alert("left------------");
+  this.x -= 5;
+};
+
+Player.prototype.moveRight = function() {
+  this.x +=5; 
+ };
+
+Player.prototype.moveUp = function() {
+  this.y -= 5;
+};
+
+Player.prototype.moveDown = function() {
+  this.y += 5;
+};
