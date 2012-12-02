2e9ebaab7375cef657ade7f4a08a3051a7fc5160
John Brannan <johnmbrannan@gmail.com>
2012-12-01 17:18:31 -0700

    added projectile.js

---
+++ public/js/projectile.js
@@ -0,0 +1,27 @@
+
+
+function projectile(x, y, rot, speed) {
+  this.x = y;
+  this.y = x;
+  this.rot = rot;
+  this.speed = speed * 4;
+  this.duration = 0;
+  this.maxDuration = 2;
+}
+
+projectile.prototype.draw = function(context) {
+   // alert("left------------");
+  ctx.fillStyle="#FFFF00";
+  context.save();
+  context.rotate(45);
+  context.fillRect(this.x, this.y, 2, 2);
+  context.restore();
+};
+
+projectile.prototype.update(elapsedTime) {
+	var radius = speed * elapsedTime;
+	this.x = radius * Math.cos(rot);
+	this.y = radius * Math.sin(rot);
+	this.duration += elapsedTime / 1000;
+}
+
