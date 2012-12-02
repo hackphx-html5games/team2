a1dea070520905be7768ff2185eb29de89475f3f
John Brannan <johnmbrannan@gmail.com>
2012-12-01 18:54:15 -0700

    player.js rotation

--- public/js/Player.js
+++ public/js/Player.js
@@ -5,33 +5,36 @@
 
 function Player() {
   this.isLeft = true;
-  this.x = 300;
-  this.y = 100;
+  this.forward = 0;
+  this.reverse = 0;
+  this.x = 200;
+  this.y = 200;
   this.rot = 0;
+  this.speed = 0;
 }
 
 Player.prototype.draw = function(context) {
   this.update()
    // alert("left------------");
   ctx.fillStyle="#00FFFF";
-  context.fillRect(this.x, this.y, 32, 32);
+  context.fillRect(this.x, this.y, 16, 32);
 };
 
 Player.prototype.moveLeft = function() {
-  this.isLeft = true;
+  this.rot -= 3;
 
 };
 
 Player.prototype.moveRight = function() {
-  this.isLeft =  false;
+  this.rot +=  3;
  };
 
 Player.prototype.moveUp = function() {
-  this.y -= 5;
+  this.speed += 5;
 };
 
 Player.prototype.moveDown = function() {
-  this.y += 5;
+  this.speed -= 5;
 };
 
 Player.prototype.shoot = function() {
@@ -41,12 +44,14 @@ Player.prototype.shoot = function() {
 	return bullet;
 };
 Player.prototype.update = function(){
-     if(this.isLeft == true){
+     /*if(this.isLeft == true){
       if(this.x > 130){
        this.x -= 1;
       }
      }else{
       if(this.x < 900-60)
       this.x += 1;
-     }
+     }*/
+	this.x = this.xspeed * Math.cos(this.rot*Math.PI/180);
+	this.y = this.yspeed * Math.sin(this.rot*Math.PI/180);
  };
