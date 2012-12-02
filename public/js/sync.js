ad9861edbfb9d2e9fb3f300e0811ffe8cdb60784
Roly Fentanes <roly426@gmail.com>
2012-12-01 17:18:35 -0700

    more methods around socket events

--- public/js/sync.js
+++ public/js/sync.js
@@ -2,6 +2,46 @@
 var socket = io.connect();
 
 socket.on('connect', function() {
-  socket.emit('play');
   console.log('woohoo!');
 });
+
+
+/**
+ * Events from the server to the client.
+ */
+socket.on('update', function(userid, position) {
+});
+
+
+socket.on('spawn', function(userid, position) {
+});
+
+
+socket.on('disconnect', function(userid} {
+});
+
+
+socket.on('shoot', function(userid, x, y, xspeed, yspeed, angle) {
+});
+
+
+socket.on('kill', function(userid) {
+});
+
+
+/**
+ * Events from the client, one player, to the server.
+ */
+function update(position) {
+  socket.emit('update', position);
+}
+
+
+function shoot(angle) {
+  socket.emit('shoot', angle);
+}
+
+
+function kill(userid) {
+  socket.emit('kill', userid);
+}
